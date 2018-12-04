125b13b7d7826304e1b8d89ead3903d72039e2c7
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-12-04 15:33:17 +0530

    Implement getNLines function

--- src/lib.js
+++ src/lib.js
@@ -2,4 +2,8 @@ const execute = function(functionName, args) {
   return functionName(args[0],args[1]);
 }
 
-module.exports = { execute };
+const getNLines = function(content,numOfLines=10) { 
+  return content.split("\n").slice(0,numOfLines).join("\n");
+}
+module.exports = { execute,
+getNLines };
--- test/libTest.js
+++ test/libTest.js
@@ -1,5 +1,6 @@
 const assert = require("assert");
-const { execute } = require("../src/lib.js");
+const { execute, 
+  getNLines } = require("../src/lib.js");
 
 const add = num => num+10;
 const sub = (num1,num2) => num1-num2;
@@ -13,3 +14,28 @@ describe("execute",function() {
   });
 });
 
+describe("getNLines",function() {
+  let contents = "AB\nCD\nEF\nGH\nIJ\nKL\nMN\nOP\nQR\nST\nUV\nWX\nYZ"
+
+  it("should return 10 lines when number of lines is not specified ",function() {
+    let expectedOutput = "AB\nCD\nEF\nGH\nIJ\nKL\nMN\nOP\nQR\nST";
+    assert.equal(getNLines(contents),expectedOutput);
+  });
+
+  it("should return specified number of lines when number of lines is given ",function() { 
+    let expectedOutput = "AB\nCD\nEF";
+    assert.equal(getNLines(contents,3),expectedOutput);
+  });
+
+  it("should return empty string when number of line is 0",function() { 
+    let expectedOutput = "";
+    assert.equal(getNLines(contents,0),expectedOutput);
+  });
+  
+  it("should return empty string when contents is empty string ",function() {
+    let contents = "";
+    let expectedOutput = "";
+    assert.equal(getNLines(contents,5),expectedOutput);
+  });
+
+});
