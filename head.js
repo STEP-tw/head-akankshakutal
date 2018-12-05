2fb66bf30b2e02211f264aefb1ca665e3ba48d64
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-12-05 09:34:41 +0530

    Implement main function in head.js

--- head.js
+++ head.js
@@ -13,6 +13,16 @@
   node ./head.js -c5 file1 file2
   node ./head.js -c 5 file1 file2
 */
+const { extractInput,execute } = require("./src/lib.js");
+const { readFileSync } = require("fs");
 
+const main = function() { 
+  let userInputs = process.argv.slice(2);
+  let parameters = extractInput(userInputs);
+  let contents = execute(readFileSync,parameters.files); 
+  let result = parameters.option(contents[0],parameters.count);
+  console.log(result);
+}
 
+main();
 
