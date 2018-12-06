7c8effd03fa7de5097dcafad10000e405dcfbfed
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-12-06 23:03:34 +0530

    Refactor

--- src/headLib.js
+++ src/headLib.js
@@ -1,96 +1,107 @@
-const getNLines = function(content,numOfLines=10) { 
-  return content.split("\n").slice(0,numOfLines).join("\n");
-}
+const getNLines = function(content, numOfLines = 10) {
+  return content
+    .split("\n")
+    .slice(0, numOfLines)
+    .join("\n");
+};
 
-const getNBytes = function(contents,numOfBytes=10) { 
-  return contents.slice(0,numOfBytes);
-}
+const getNBytes = function(contents, numOfBytes = 10) {
+  return contents.slice(0, numOfBytes);
+};
 
-const select = function(option) { 
-  return ( (/-c/).test(option) ) ? getNBytes : getNLines;
-}
+const select = function(option) {
+  return /-c/.test(option) ? getNBytes : getNLines;
+};
 
-const isNumber = (value) => value.match(/^-[0-9]/g);
+const isNumber = value => value.match(/^-[0-9]/g);
 
-const isValidType = (value) => value.match(/^-[nc]/g);
+const isValidType = value => value.match(/^-[nc]/g);
 
-const isValidOption = (value) => value.match(/^-[nc][0-9]/g);
+const isValidOption = value => value.match(/^-[nc][0-9]/g);
 
-const getCount = function(args) { 
-  if( !isValidType(args[0]) &&  !isNumber(args[0]) ) {
+const getCount = function(args) {
+  if (!isValidType(args[0]) && !isNumber(args[0])) {
     return 10;
   }
-  if(isNumber(args[0])) {
-    return args[0].slice(1,args[0].length);
+  if (isNumber(args[0])) {
+    return args[0].slice(1, args[0].length);
   }
-  if(isValidOption(args[0])) {
-    return args[0].slice(2,args[0].length);
+  if (isValidOption(args[0])) {
+    return args[0].slice(2, args[0].length);
   }
-  return args[1]; 
-}
+  return args[1];
+};
 
-const getFileNames = function(args) { 
+const getFileNames = function(args) {
   let sliceCount = 0;
-  if( args[0][0] == "-") {
-    sliceCount =1;
+  if (args[0][0] == "-") {
+    sliceCount = 1;
   }
-  if(isFinite(args[1])) {
-    sliceCount =2;
+  if (isFinite(args[1])) {
+    sliceCount = 2;
   }
   return args.slice(sliceCount);
-}
+};
 
-const parse = function(args) { 
-  return { option : select(args[0]),
-    count : getCount(args.slice(0,2)),
-    files : getFileNames(args)
-  }
-}
+const parse = function(args) {
+  return {
+    option: select(args[0]),
+    count: getCount(args.slice(0, 2)),
+    files: getFileNames(args)
+  };
+};
 
-const addHeading = function(fileName,content) { 
-    return "==> "+ fileName + " <==\n"+ content;
-}
+const addHeading = function(fileName, content) {
+  return "==> " + fileName + " <==\n" + content;
+};
 
-const format = function(fileName,contents) { 
-  return  addHeading(fileName,contents);
-}
+const format = function(fileName, contents) {
+  return addHeading(fileName, contents);
+};
 
-const isValid  = function(args,userInput,fileSystem) { 
+const isValid = function(args, userInput, fileSystem) {
   const invalidLineCount = "head: illegal line count -- ";
   const invalidByteCount = "head: illegal byte count -- ";
-  const errorMessage = 'head: illegal option -- ';
-  const usageMessage = 'usage: head [-n lines | -c bytes] [file ...]';
+  const errorMessage = "head: illegal option -- ";
+  const usageMessage = "usage: head [-n lines | -c bytes] [file ...]";
 
-  if( userInput.count < 1 || isNaN(userInput.count)) {
-    return (userInput.option == getNBytes ) ? invalidByteCount+userInput.count : invalidLineCount+userInput.count;
+  if (userInput.count < 1 || isNaN(userInput.count)) {
+    return userInput.option == getNBytes
+      ? invalidByteCount + userInput.count
+      : invalidLineCount + userInput.count;
   }
-  if( !isValidType(args[0]) && args[0] != userInput.files[0] && !isNumber(args[0]) ) {
-    return errorMessage+args[0].slice(0)+"\n"+usageMessage;
+  if (
+    !isValidType(args[0]) &&
+    args[0] != userInput.files[0] &&
+    !isNumber(args[0])
+  ) {
+    return errorMessage + args[0].slice(0) + "\n" + usageMessage;
   }
-  let formatContents = getContents.bind(null,fileSystem,userInput);
+  let formatContents = getContents.bind(null, fileSystem, userInput);
   let formattedContents = userInput.files.map(formatContents);
   return formattedContents.join("\n\n");
-}
+};
 
-const getContents  = function (fileSystem,userInput,file) {
-  if( !fileSystem.existsSync(file) ) {
-    return 'head: '+file+': No such file or directory';
+const getContents = function(fileSystem, userInput, file) {
+  if (!fileSystem.existsSync(file)) {
+    return "head: " + file + ": No such file or directory";
   }
-  let contents = fileSystem.readFileSync(file,"utf8"); 
-  let requiredContents = userInput.option(contents,userInput.count);
-  if( userInput.files.length == 1) {
+  let contents = fileSystem.readFileSync(file, "utf8");
+  let requiredContents = userInput.option(contents, userInput.count);
+  if (userInput.files.length == 1) {
     return requiredContents;
   }
   return format(file, requiredContents);
-}
+};
 
-const head = function(args,fileSystem) { 
+const head = function(args, fileSystem) {
   let userInput = parse(args);
-  let contents = isValid(args,userInput,fileSystem);
+  let contents = isValid(args, userInput, fileSystem);
   return contents;
-}
+};
 
-module.exports = { head,
+module.exports = {
+  head,
   getNLines,
   select,
   getCount,
@@ -100,5 +111,5 @@ module.exports = { head,
   addHeading,
   getContents,
   isValid,
-  getNBytes 
+  getNBytes
 };
--- test/headLibTest.js
+++ test/headLibTest.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const { 
+const {
   getNLines,
   getContents,
   select,
@@ -10,190 +10,209 @@ const {
   format,
   addHeading,
   head,
-  getNBytes } = require("../src/headLib.js");
+  getNBytes
+} = require("../src/headLib.js");
 
-describe("getNLines",function() {
-  let contents = "AB\nCD\nEF\nGH\nIJ\nKL\nMN\nOP\nQR\nST\nUV\nWX\nYZ"
+describe("getNLines", function() {
+  let contents = "AB\nCD\nEF\nGH\nIJ\nKL\nMN\nOP\nQR\nST\nUV\nWX\nYZ";
 
-  it("should return 10 lines when number of lines is not specified ",function() {
+  it("should return 10 lines when number of lines is not specified ", function() {
     let expectedOutput = "AB\nCD\nEF\nGH\nIJ\nKL\nMN\nOP\nQR\nST";
-    assert.equal(getNLines(contents),expectedOutput);
+    assert.equal(getNLines(contents), expectedOutput);
   });
 
-  it("should return specified number of lines when number of lines is given ",function() { 
+  it("should return specified number of lines when number of lines is given ", function() {
     let expectedOutput = "AB\nCD\nEF";
-    assert.equal(getNLines(contents,3),expectedOutput);
+    assert.equal(getNLines(contents, 3), expectedOutput);
   });
 
-  it("should return empty string when number of line is 0",function() { 
+  it("should return empty string when number of line is 0", function() {
     let expectedOutput = "";
-    assert.equal(getNLines(contents,0),expectedOutput);
+    assert.equal(getNLines(contents, 0), expectedOutput);
   });
-  
-  it("should return empty string when contents is empty string ",function() {
+
+  it("should return empty string when contents is empty string ", function() {
     let contents = "";
     let expectedOutput = "";
-    assert.equal(getNLines(contents,5),expectedOutput);
+    assert.equal(getNLines(contents, 5), expectedOutput);
   });
-
 });
 
-describe("getNBytes",function() {
-  let contents = "AB\nCD\nEF\nGH\nIJ\nKL\nMN\nOP\nQR\nST\nUV\nWX\nYZ"
+describe("getNBytes", function() {
+  let contents = "AB\nCD\nEF\nGH\nIJ\nKL\nMN\nOP\nQR\nST\nUV\nWX\nYZ";
 
-  it("should return 10 bytes/characters when number of bytes is not specified",function() {
+  it("should return 10 bytes/characters when number of bytes is not specified", function() {
     let expectedOutput = "AB\nCD\nEF\nG";
-    assert.equal(getNBytes(contents),expectedOutput);
+    assert.equal(getNBytes(contents), expectedOutput);
   });
 
-  it("should return specified number of bytes when number of bytes is given ",function() { 
+  it("should return specified number of bytes when number of bytes is given ", function() {
     let expectedOutput = "AB\n";
-    assert.equal(getNBytes(contents,3),expectedOutput);
+    assert.equal(getNBytes(contents, 3), expectedOutput);
   });
 
-  it("should return empty string when number of bytes is 0",function() { 
+  it("should return empty string when number of bytes is 0", function() {
     let expectedOutput = "";
-    assert.equal(getNBytes(contents,0),expectedOutput);
+    assert.equal(getNBytes(contents, 0), expectedOutput);
   });
-  
-  it("should return empty string when contents is empty string ",function() {
+
+  it("should return empty string when contents is empty string ", function() {
     let contents = "";
     let expectedOutput = "";
-    assert.equal(getNBytes(contents,5),expectedOutput);
-  });
-
-});
-
-describe("parse",function() {
-
-  describe("select",function() {
-
-    it("should return getNBytes function when input is -c",function() {
-      assert.equal(select("-c"),getNBytes);
-      assert.equal(select("-c5"),getNBytes);
-    });
-
-    it("should return getNBytes function when input is rather than -c",function() {
-      assert.equal(select("-n"),getNLines);
-      assert.equal(select("-n5"),getNLines)
-    });
-
-  });
-
-  describe("getCount",function() {
-
-    it("should return 10 when -n/-c is not given ",function() {
-      assert.equal(getCount(["-d","File1"]),10);
-    });
-
-    it("should return specified number from that string ",function() { 
-      assert.equal(getCount(["-n20", "file1"]),20);
-    });
-
-    it("should return number that specified in 1 index",function() { 
-      assert.equal(getCount(["-c","20"]),20);
-    });
-
-    it("should return number that specified in 0 index",function() { 
-      assert.equal(getCount(["-20","File1"]),20);
-    });
-  });
-
-  describe("getFileNames",function() {
-
-    it("should return array slice by 1 when input contains /-[0-9]/ ",function() {
-      assert.deepEqual(getFileNames(["-n4","File1","File2"]),["File1","File2"]);
-    });
-
-    it("should return array slice by 2 when input does not contains any number",function() { 
-      assert.deepEqual(getFileNames(["-n","10","File1","File2"]),["File1","File2"]);
-    });
-
-    it("should return given array when input doesn't contains - and any number",function() { 
-      assert.deepEqual(getFileNames(["Hello","Hiiii","Welcome"]),["Hello","Hiiii","Welcome"]);
-    });
-
-  });
-
-  it("should return object that contains three keys", function () {
-    assert.deepEqual(parse(["-n","4","File2","File3"]),{ option : getNLines, count : 4 , files : ["File2","File3"] });
-
-    assert.deepEqual(parse(["-n4","File1","File2","File3"]),{ option : getNLines, count : 4 , files : ["File1","File2","File3"] });
-
+    assert.equal(getNBytes(contents, 5), expectedOutput);
   });
 });
 
-describe("format",function() {
-
-  describe("addHeading",function() {
-
-    it("should return first arg im between ==> <== and second on next line",function() {
-      assert.equal(addHeading("Hello","How are you ? "),"==> Hello <==\nHow are you ? ");
+describe("parse", function() {
+  describe("select", function() {
+    it("should return getNBytes function when input is -c", function() {
+      assert.equal(select("-c"), getNBytes);
+      assert.equal(select("-c5"), getNBytes);
     });
 
+    it("should return getNBytes function when input is rather than -c", function() {
+      assert.equal(select("-n"), getNLines);
+      assert.equal(select("-n5"), getNLines);
+    });
   });
 
-  it("should return array elements alternatively when first array lenth is not equal to 1", function() { 
-    let expectedOutput = "==> Hello <==\nHow are you ?"
-    assert.equal(format("Hello","How are you ?"),expectedOutput);
+  describe("getCount", function() {
+    it("should return 10 when -n/-c is not given ", function() {
+      assert.equal(getCount(["-d", "File1"]), 10);
+    });
+
+    it("should return specified number from that string ", function() {
+      assert.equal(getCount(["-n20", "file1"]), 20);
+    });
+
+    it("should return number that specified in 1 index", function() {
+      assert.equal(getCount(["-c", "20"]), 20);
+    });
+
+    it("should return number that specified in 0 index", function() {
+      assert.equal(getCount(["-20", "File1"]), 20);
+    });
   });
 
+  describe("getFileNames", function() {
+    it("should return array slice by 1 when input contains /-[0-9]/ ", function() {
+      assert.deepEqual(getFileNames(["-n4", "File1", "File2"]), [
+        "File1",
+        "File2"
+      ]);
+    });
+
+    it("should return array slice by 2 when input does not contains any number", function() {
+      assert.deepEqual(getFileNames(["-n", "10", "File1", "File2"]), [
+        "File1",
+        "File2"
+      ]);
+    });
+
+    it("should return given array when input doesn't contains - and any number", function() {
+      assert.deepEqual(getFileNames(["Hello", "Hiiii", "Welcome"]), [
+        "Hello",
+        "Hiiii",
+        "Welcome"
+      ]);
+    });
+  });
+
+  it("should return object that contains three keys", function() {
+    assert.deepEqual(parse(["-n", "4", "File2", "File3"]), {
+      option: getNLines,
+      count: 4,
+      files: ["File2", "File3"]
+    });
+
+    assert.deepEqual(parse(["-n4", "File1", "File2", "File3"]), {
+      option: getNLines,
+      count: 4,
+      files: ["File1", "File2", "File3"]
+    });
+  });
 });
 
-describe("getContents",function() {
-
-  let userInput = { option : getNLines, count : "1", files : ["file1"]}
-
-  it("should return error message ",function() {
-    let fileSystem = { readFileSync :() =>"Hello" , existsSync : () => false  }
-    let expectedOutput = 'head: file1: No such file or directory'
-    assert.equal(getContents(fileSystem,userInput,"file1"),expectedOutput);
+describe("format", function() {
+  describe("addHeading", function() {
+    it("should return first arg im between ==> <== and second on next line", function() {
+      assert.equal(
+        addHeading("Hello", "How are you ? "),
+        "==> Hello <==\nHow are you ? "
+      );
+    });
   });
 
-  it("should return Hello because file exists",function() {
-    let fileSystem = { readFileSync :() =>"Hello" , existsSync : () => true  }
-    let expectedOutput = 'Hello'
-    assert.equal(getContents(fileSystem,userInput,"file1"),expectedOutput);
-  });
-
-  it("should return Hello twice because file exists",function() {
-    let userInput = { option : getNLines, count : "1", files : ["file1","file2"]}
-    let fileSystem = { readFileSync :() =>"Hello" , existsSync : () => true  }
-    let expectedOutput = '==> file1 <==\nHello'
-    assert.equal(getContents(fileSystem,userInput,"file1"),expectedOutput);
+  it("should return array elements alternatively when first array lenth is not equal to 1", function() {
+    let expectedOutput = "==> Hello <==\nHow are you ?";
+    assert.equal(format("Hello", "How are you ?"), expectedOutput);
   });
 });
 
-describe("isValid",function() {
+describe("getContents", function() {
+  let userInput = { option: getNLines, count: "1", files: ["file1"] };
 
-  let userInput = { option : getNLines, count : "1", files : ["file1"]}
-  let fileSystem = { readFileSync :() =>"Hello" , existsSync : () => true  }
-
-  it("should return error message with usage message when option is invalid",function() {
-    let expectedOutput = 'head: illegal option -- -v\nusage: head [-n lines | -c bytes] [file ...]';
-    assert.equal(isValid(["-v","file1"],userInput,fileSystem),expectedOutput);
+  it("should return error message ", function() {
+    let fileSystem = { readFileSync: () => "Hello", existsSync: () => false };
+    let expectedOutput = "head: file1: No such file or directory";
+    assert.equal(getContents(fileSystem, userInput, "file1"), expectedOutput);
   });
 
-  it("should return Hello message when option is valid",function() {
-    let fileSystem = { readFileSync :() =>"Hello" , existsSync : () =>true  }
-    let expectedOutput = 'Hello';
-    assert.equal(isValid(["-n","file1"],userInput,fileSystem),expectedOutput);
+  it("should return Hello because file exists", function() {
+    let fileSystem = { readFileSync: () => "Hello", existsSync: () => true };
+    let expectedOutput = "Hello";
+    assert.equal(getContents(fileSystem, userInput, "file1"), expectedOutput);
   });
 
-  it("should return Hello message when option is valid",function() {
-    let fileSystem = { readFileSync :() =>"Hello" , existsSync : () =>true  }
-    let expectedOutput = 'Hello';
-    assert.equal(isValid(["-c0","file1"],userInput,fileSystem),expectedOutput);
+  it("should return Hello twice because file exists", function() {
+    let userInput = {
+      option: getNLines,
+      count: "1",
+      files: ["file1", "file2"]
+    };
+    let fileSystem = { readFileSync: () => "Hello", existsSync: () => true };
+    let expectedOutput = "==> file1 <==\nHello";
+    assert.equal(getContents(fileSystem, userInput, "file1"), expectedOutput);
   });
-
 });
 
-describe("head",function() {
-  const fileSystem = { readFileSync :() =>"Hello" , existsSync : () => false  }
+describe("isValid", function() {
+  let userInput = { option: getNLines, count: "1", files: ["file1"] };
+  let fileSystem = { readFileSync: () => "Hello", existsSync: () => true };
 
-  it('should return error message when input file is not present ', function() {
-    let expectedOutput = 'head: illegal line count -- 0';
-    assert.deepEqual(head(["-n0","File1"],fileSystem),expectedOutput);
+  it("should return error message with usage message when option is invalid", function() {
+    let expectedOutput =
+      "head: illegal option -- -v\nusage: head [-n lines | -c bytes] [file ...]";
+    assert.equal(
+      isValid(["-v", "file1"], userInput, fileSystem),
+      expectedOutput
+    );
   });
 
+  it("should return Hello message when option is valid", function() {
+    let fileSystem = { readFileSync: () => "Hello", existsSync: () => true };
+    let expectedOutput = "Hello";
+    assert.equal(
+      isValid(["-n", "file1"], userInput, fileSystem),
+      expectedOutput
+    );
+  });
+
+  it("should return Hello message when option is valid", function() {
+    let fileSystem = { readFileSync: () => "Hello", existsSync: () => true };
+    let expectedOutput = "Hello";
+    assert.equal(
+      isValid(["-c0", "file1"], userInput, fileSystem),
+      expectedOutput
+    );
+  });
+});
+
+describe("head", function() {
+  const fileSystem = { readFileSync: () => "Hello", existsSync: () => false };
+
+  it("should return error message when input file is not present ", function() {
+    let expectedOutput = "head: illegal line count -- 0";
+    assert.deepEqual(head(["-n0", "File1"], fileSystem), expectedOutput);
+  });
 });
