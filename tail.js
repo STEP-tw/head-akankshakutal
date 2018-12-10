758ccb7dbac2437ab0ce9a99608bd1e5bfeda9b0
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-12-10 22:50:43 +0530

    Implement tail

--- tail.js
+++ tail.js
@@ -1,18 +1,11 @@
-/* 
-  Usage:
-  node ./tail.js file1
-  node ./tail.js -n5 file1
-  node ./tail.js -n 5 file1
-  node ./tail.js -5 file1
-  node ./tail.js file1 file2
-  node ./tail.js -n 5 file1 file2
-  node ./tail.js -n5 file1 file2
-  node ./tail.js -5 file1 file2 
-  node ./tail.js -c5 file1
-  node ./tail.js -c 5 file1
-  node ./tail.js -c5 file1 file2
-  node ./tail.js -c 5 file1 file2
-*/
+const { head } = require("./src/headLib.js");
+const fs = require("fs");
 
+const main = function() { 
+  let args = process.argv.slice(2);
+  let result = head(args,fs,process.argv[1]);
+  console.log(result);
+}
 
+main();
 
