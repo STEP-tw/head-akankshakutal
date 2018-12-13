6962c1956e43ed9cebcb5f9db2dba53cfb78822e
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-12-13 22:41:39 +0530

    Modify GetNLine and GetNByte

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,4 @@
 const getNLines = function(content, range) {
-  if (range[0] === 0 && range[1] == undefined) return "";
   return content
     .split("\n")
     .slice(range[0], range[1])
@@ -7,7 +6,6 @@ const getNLines = function(content, range) {
 };
 
 const getNBytes = function(content, range) {
-  if (range[0] === 0 && range[1] == undefined) return "";
   return content.slice(range[0], range[1]);
 };
 
